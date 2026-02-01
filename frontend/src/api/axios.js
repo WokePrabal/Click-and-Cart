a606f090aaabcd6ccc93c2df5ee1116805d16cd2
WokePrabal <prabalpratap465@gmail.com>
2026-02-01 22:16:40 +0530

    few changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.PROD
-    ? 'https://click-and-cart-1.onrender.com'
-    : 'http://localhost:5000',
+    ? import.meta.env.VITE_API_BASE_URL
+    : 'http://localhost:5000/api',
   withCredentials: true,
 });
 
